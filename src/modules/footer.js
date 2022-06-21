dd613173febe59dcc45f3b31b8b06cb66b1c6abc
Mohamed Jadib <jadibdev@gmail.com>
2022-06-21 10:42:21 +0100

    Update company name in footer

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -3,7 +3,7 @@ import '../style.css';
 const content = document.getElementById('content');
 
 const footer = `
-<div class="footer">&copy Meals, Inc All Rights Reserved 2022</div>`;
+<div class="footer">&copy King Meals, Inc. All Rights Reserved 2022</div>`;
 
 const renderFooter = () => {
   content.insertAdjacentHTML('beforeend', footer);
