528d94f8a9a450770f90cde196137ac61de83ad1
Mohamed Jadib <jadibdev@gmail.com>
2022-06-22 13:47:21 +0100

    Implement add new like

--- src/modules/getMeals.js
+++ src/modules/getMeals.js
@@ -8,6 +8,8 @@ const ids = [
   53009,
 ];
 
+const InvolvementApiKey = 'H1778Eipl0PRUFSJ8DNo';
+
 const content = document.getElementById('content');
 
 const ul = document.createElement('ul');
@@ -17,20 +19,72 @@ const getMeals = async () => {
   for (let i = 0; i < ids.length; i += 1) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ids[i]}`);
     const data = await response.json();
+    console.log(data);
     const mealTemplate = `
     <li>   
       <img class="meal-images" src=${data.meals[0].strMealThumb} alt="meal pic" />
-      <div class="title-and-icons">
+      <article class="title-and-icons">
         <h5>${data.meals[0].strMeal}</h5>
-        <div>
-          <i class='fa fa-heart'></i>
+        <div id="like-container">
+          <i id=${data.meals[0].strMeal} class='fa fa-heart'></i>
           <p>4</p>
         </div>
-      </div>
+      </article>
       <button>Comments</button>
     </li>`;
     ul.insertAdjacentHTML('beforeend', mealTemplate);
   }
+
+  // event listener to post a like
+  document.querySelectorAll('.fa-heart').forEach((item) => {
+    item.addEventListener('click', (event) => {
+      console.log(item.id);
+
+      async function postLike() {
+        const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${InvolvementApiKey}/likes/`;
+
+        const data = {
+          item_id: item.id,
+        };
+
+        const param = {
+
+          method: 'Post',
+
+          headers: {
+
+            'content-type': 'application/json;charset = UTF-8',
+
+          },
+
+          body: JSON.stringify(data),
+
+        };
+
+        const sendRequest = await fetch(url, param);
+
+        const response = await sendRequest.text();
+
+        return response;
+      }
+
+      postLike().then((data) => {
+        console.log('back from server', data);
+      });
+
+      async function getLikes() {
+        const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${InvolvementApiKey}/likes`);
+        const data = await response.json();
+        return data;
+      }
+
+      getLikes()
+        .then((data) => {
+          console.log(data);
+          item.innerHTML = '<p>something</p>';
+        });
+    });
+  });
 };
 
 export default getMeals;
