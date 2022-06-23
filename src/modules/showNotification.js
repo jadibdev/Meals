4023df6826501e786a723f346998b22d1999ee6a
Profsain <profsaincrown@gmail.com>
2022-06-23 19:53:46 +0100

    fix linters error

--- src/modules/showNotification.js
+++ src/modules/showNotification.js
@@ -2,7 +2,7 @@ const showNotification = () => {
   const notificationElem = document.querySelector('.notification');
 
   notificationElem.innerHTML = '...Comment Added..';
-  setTimeout(() => { 
+  setTimeout(() => {
     notificationElem.innerHTML = '';
   }, 3000);
 };
